a1073cc15fca622937a90cf89e133628f13675e0
Macascript <jotaele.arrojo@gmail.com>
2023-02-05 10:56:26 +0100

    eajeafia

--- RootsGame/Assets/Scripts/GusanoBehaviour.cs
+++ RootsGame/Assets/Scripts/GusanoBehaviour.cs
@@ -75,6 +75,7 @@ public class GusanoBehaviour : MonoBehaviour
         if (o is Root)
         {
             Debug.Log("OSTIA RAMA");
+            sentido *= -1;
             pointsIndex += (2 * sentido) % pointsPrefab.transform.childCount;
         }
         transform.DOMove(pointsPrefab.transform.GetChild(pointsIndex).position, Duration(pointsPrefab.transform.GetChild(pointsIndex).position)).SetEase(Ease.Linear).OnComplete(NextStep);
@@ -109,8 +110,8 @@ public class GusanoBehaviour : MonoBehaviour
         //        anim.SetInteger("index", 0);
         //        break;
         //}
-        pointsIndex += (1 * sentido) % pointsPrefab.transform.childCount;
-        Debug.Log("Hemos llegao al punto, siguiente: " + pointsIndex);
+        pointsIndex += (sentido) % pointsPrefab.transform.childCount;
+        Debug.Log("Hemos llegao al punto, siguiente: " + pointsIndex+" y somos "+name);
         steps++;
         // TODO: Decirle al código de jaime que ya no estoy aquí, que estoy allí
     }
