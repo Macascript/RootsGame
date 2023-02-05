bb28e4bebc2b4b155374ff417aae5b1e99e34a76
Jaime De Carvajal Morillo <madmovesl@gmail.com>
2023-02-05 14:00:49 +0100

    arbol

--- RootsGame/Assets/Scripts/UI/ArbolNivel.cs
+++ RootsGame/Assets/Scripts/UI/ArbolNivel.cs
@@ -8,11 +8,33 @@ public class ArbolNivel : MonoBehaviour
     [SerializeField]
     private Sprite[] arboles;
 
+    [SerializeField]
+    private AudioSource m_source;
+
+    private int index = 0;
+    private bool levelUp = false;
+
     private void Update()
     {
-        if (GridManager.instance.GetGridIndex(GridManager.instance.player.transform.position) > 161)
-            GetComponent<Image>().sprite = arboles[1];
+        if (GridManager.instance.GetGridIndex(GridManager.instance.player.transform.position) > 161 && levelUp)
+        {
+            levelUp = true;
+            index = 1;
+            if(levelUp)
+            m_source.Play();
+            StartCoroutine(changeLevel());
+        }    
         if (GridManager.instance.GetGridIndex(GridManager.instance.player.transform.position) > 363)
-            GetComponent<Image>().sprite = arboles[2];
+        {
+            index = 2;
+            m_source.Play();
+        }      
+    }
+
+    private IEnumerator changeLevel()
+    {
+        //sol
+        yield return new WaitForSeconds(5);
+        GetComponent<Image>().sprite = arboles[index];
     }
 }
